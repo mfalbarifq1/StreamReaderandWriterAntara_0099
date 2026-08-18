9f4f5c3e0a194daf78101cf2fe90342d612119b0
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-08-18 10:18:36 +0700

    kondisi jika file ada

--- iowithpath.cpp
+++ iowithpath.cpp
@@ -39,3 +39,15 @@ outfile.close();
 //membuka file dalam mode membaca
 ifstream infile;
 
+//menunjuk ke sebuah file dengan ekstensi .txt
+infile.open(NamaFile + ".txt", ios :: in);
+
+cout << endl << ">= Membuka dan membaca file" << endl;
+//kondisi jika file ada
+if (infile.is_open())
+{
+    //perulangan untuk menampilkan setiap baris
+    while (getline(infile, baris))
+    {
+        cout << baris << endl;
+    }
